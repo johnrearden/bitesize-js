6e6d9f5e04485dde3f95d8538aa7b9dbc06a191e
John Rearden <isnocturnal@gmail.com>
2024-10-17 14:42:11 +0000

    Add comments to code in select_element_update.js

--- assets/js/select_element_update.js
+++ assets/js/select_element_update.js
@@ -1,3 +1,4 @@
+// The possible colours for each type of vehicle
 const COLOURS = {
     car: [
         "Dusky Rose", "Moonlit Pearl", "Lavender Mist", "Burgundy Bliss",
@@ -6,10 +7,14 @@ const COLOURS = {
     van: ["White", "Navy"],
 };
 
+// Event listener fires when the HTML content has loaded.
 document.addEventListener("DOMContentLoaded", () => {
+
+    // Get references to the vehicle radio buttons and the select element
     const vehicleButtons = document.getElementsByName("vehicle-type");
     const colourSelector = document.getElementById("colour-selector");
 
+    // Add an event listener, listening for the 'change' event, to each button
     for (let button of vehicleButtons) {
         button.addEventListener("change", () => {
             const vehicle = button.id.split("-")[0];
@@ -17,13 +22,21 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
+    /**
+     * A function that accepts a vehicle and populates the select options
+     * 
+     * @param {string} vehicle 
+     */
     const updateColourOptions = (vehicle) => {
 
+        // Remove all but the first (informational) option
         const colours = COLOURS[vehicle];
         while (colourSelector.options.length > 1) {
             colourSelector.remove(1);
         }
 
+        // Iterate through the list of colours, and add a new option to the
+        // select element for each one.
         for (let colour of colours) {
             const option = document.createElement("option");
             option.value = colour;
@@ -31,9 +44,11 @@ document.addEventListener("DOMContentLoaded", () => {
             colourSelector.add(option, null);
         }
 
+        // Update the vehicle name in the first option (the info option)
         const infoOption = colourSelector.options[0];
         infoOption.text = "Choose a colour for your " + vehicle;
     }
 
+    // Run our function when the page loads to first populate the select element
     updateColourOptions("car");
 });

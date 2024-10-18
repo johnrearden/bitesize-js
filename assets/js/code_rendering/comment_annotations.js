cd044b5bf3ceb003a44913e7918d7705960c6739
John Rearden <isnocturnal@gmail.com>
2024-10-18 23:06:22 +0000

    Change container to container-fluid in both existing html example files to allow full width code, and add HTML source code section

--- assets/js/code_rendering/comment_annotations.js
+++ assets/js/code_rendering/comment_annotations.js
@@ -1,12 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-
     const url = document.querySelector('[type="hidden"]').value;
     console.log(url);
 
     fetch(url)
-            .then((response) => response.text())
-            .then((code) => {
-                document.querySelector('#code-display').innerHTML = code;
-                Prism.highlightAll();
-            });
+        .then((response) => response.text())
+        .then((code) => {
+            document.querySelector("#code-display-js").innerHTML = code;
+            Prism.highlightAll();
+        });
+
+    fetch(window.location.href)
+        .then((response) => response.text())
+        .then((code) => {
+            document.querySelector("#code-display-html").textContent = code;
+            Prism.highlightAll();
+        });
+
+    
+    
 });
